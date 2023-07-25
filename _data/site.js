bcb8465db053f319fa36c280c97059d99acc97e0
James Docherty <james@jmd.is>
2023-07-25 22:00:43 +0100

    Trying to fix build

--- _data/site.js
+++ _data/site.js
@@ -1,20 +1,20 @@
 module.exports = {
   meta: {
-    title: "James Docherty",
+    title: "Digital Garden",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     lang: "en",
-    siteUrl: "jmd.is",
+    siteUrl: "https://example.com/",
   },
   feed: { // used in feed.xml.njk
     subtitle: "Lorem ipsum dolor sit amet consecuteor",
     filename: "atom.xml",
     path: "/atom.xml",
-    id: "https://jmd.is",
+    id: "https://example.com/",
     authorName: "John Doe",
-    authorEmail: "james@jmd.is"
+    authorEmail: "johndoe@example.com"
   },
   hero: { // used in hero section of main page ie. index.html.njk
-    title: "Welcome to my digital garden",
-    description: "A place to share my half-baked ideas and notes &amp; cruft gathered on my journey through the internet &amp; beyond,
+    title: "Welcome to my purple blog",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusantium deserunt odio esse."
   }
 }
